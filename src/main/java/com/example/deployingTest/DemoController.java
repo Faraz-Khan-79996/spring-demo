e1a0b26606f8bb2c2c1fdff33cdf132cdea4fd88
Faraz-Khan-79996 <farazgod1234@gmail.com>
2025-06-29 19:57:33 +0530

    added a post method.

--- src/main/java/com/example/deployingTest/DemoController.java
+++ src/main/java/com/example/deployingTest/DemoController.java
@@ -1,12 +1,27 @@
 package com.example.deployingTest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class DemoController {
     @GetMapping("/")
     public String home() {
         return "Hello from Spring Boot on Render!";
+    }
+
+    // Simple POST endpoint
+    @PostMapping("/greet")
+    public Map<String, String> greetUser(@RequestBody Map<String, String> request) {
+        String name = request.getOrDefault("name", "Guest");
+
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Hello, " + name + "!");
+        return response;
     }
 }
